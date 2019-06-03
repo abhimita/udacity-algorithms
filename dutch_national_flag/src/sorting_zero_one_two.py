a9de2b386771029937fe76c7237a83cf358b28f3
abhijitbhatt <bhattacharya.abhi94@gmail.com>
2019-06-02 21:00:25 -0700

    Added comment and change verbose level of unit tests

--- dutch_national_flag/src/sorting_zero_one_two.py
+++ dutch_national_flag/src/sorting_zero_one_two.py
@@ -1,9 +1,19 @@
-
+"""
+Class that implements sorting of elements
+"""
 class ZeroOneTwoSorter:
+    # Sort is a static method so there is no need to create an instance of the class
     @staticmethod
     def sort(array):
+        # There are three pointers: zero_ptr, one_ptr & two_ptr
+        # zero_ptr & one_ptr are initialized to the starting of the array
+        # two_ptr points to end of the array
         zero_ptr = one_ptr = 0
         two_ptr = len(array) - 1
+        # Elements between one_ptr & two_ptr can be of three types
+        # element == 0, swap it with element pointed by zero_ptr . Advance both zero & one_ptr
+        # element == 1, just advance one_ptr - no swapping is needed
+        # element == 2, swap it with element pointed to by two_ptr. Advance two_ptr
         while one_ptr <= two_ptr:
             if array[one_ptr] == 0:
                 array[zero_ptr], array[one_ptr] = array[one_ptr], array[zero_ptr]
@@ -15,6 +25,3 @@ class ZeroOneTwoSorter:
                 array[one_ptr], array[two_ptr] = array[two_ptr], array[one_ptr]
                 two_ptr -= 1
         return array
-
-if __name__ == '__main__':
-    print(ZeroOneTwoSorter.sort([0, 0, 2, 2, 2, 1, 1, 1, 2, 0, 2]))
--- dutch_national_flag/test/test_sorting_zero_one_two.py
+++ dutch_national_flag/test/test_sorting_zero_one_two.py
@@ -2,9 +2,11 @@ import unittest
 from sorting_zero_one_two import ZeroOneTwoSorter
 
 class TestZeroOneTwoSorter(unittest.TestCase):
+    # Array to be sorted as few zeros at the beginning and 2 at the end
     def test_sort_with_array_starting_with_zero_ending_with_two(self):
         self.assertEqual(ZeroOneTwoSorter.sort([0, 0, 2, 2, 2, 1, 1, 1, 2, 0, 2]), [0, 0, 0, 1, 1, 1, 2, 2, 2, 2, 2])
 
+    # Array to be sorted starts with 2 and ends with 1
     def test_sort_with_array_starting_with_two_ending_with_one(self):
         self.assertEqual(
             ZeroOneTwoSorter.sort(
@@ -13,7 +15,7 @@ class TestZeroOneTwoSorter(unittest.TestCase):
             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2]
         )
 
-
+    # Array is already sorted
     def test_sort_already_sorted_array(self):
         self.assertEqual(
             ZeroOneTwoSorter.sort(
@@ -22,5 +24,11 @@ class TestZeroOneTwoSorter(unittest.TestCase):
             [0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2]
         )
 
+    # Array is to be sorted is empty (no elements)
+    def test_sort_with_empty_array(self):
+        self.assertEqual(ZeroOneTwoSorter.sort([]), [])
+
 if __name__ == '__main__':
-    unittest.main()
+    suite = unittest.TestLoader().loadTestsFromTestCase(TestZeroOneTwoSorter)
+    unittest.TextTestRunner(verbosity=2).run(suite)
+    #unittest.main()
