695cba6fc81a81e748559138e753d5c37e7ac258
abhijitbhatt <bhattacharya.abhi94@gmail.com>
2019-06-04 14:27:42 -0700

    Added more test cases

--- sqrt/test/test_sqrt.py
+++ sqrt/test/test_sqrt.py
@@ -2,12 +2,17 @@ from sqrt import Sqrt
 import unittest
 
 class TestSqrt(unittest.TestCase):
-    def test_sqrt(self):
+    # Test when input number is perfect square
+    def test_sqrt_for_perfect_square(self):
         self.assertEqual("Pass" if (5 == Sqrt.sqrt(25)) else "Fail", "Pass")
+    # Test when input number is not perfect square
+    def test_sqrt_when_number_is_not_perfect_square(self):
         self.assertEqual("Pass" if (5 == Sqrt.sqrt(27)) else "Fail", "Pass")
-        self.assertEqual("Pass" if (4 == Sqrt.sqrt(16)) else "Fail", "Pass")
+    # Test when input number is zero
+    def test_sqrt_when_number_is_zero(self):
         self.assertEqual("Pass" if  (0 == Sqrt.sqrt(0)) else "Fail", "Pass")
-        self.assertEqual("Pass" if  (3 == Sqrt.sqrt(9)) else "Fail", "Pass")
+    def test_sqrt_when_number_is_smallest_prime(self):
+        self.assertEqual("Pass" if  (1 == Sqrt.sqrt(2)) else "Fail", "Pass")
 
 if __name__ == '__main__':
     suite = unittest.TestLoader().loadTestsFromTestCase(TestSqrt)
