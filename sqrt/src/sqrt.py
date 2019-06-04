0b8cf91f3d6863af4cbdcca9dab4f9bd639bf9e7
abhijitbhatt <bhattacharya.abhi94@gmail.com>
2019-06-04 14:26:21 -0700

    Added doc string

--- sqrt/src/sqrt.py
+++ sqrt/src/sqrt.py
@@ -1,10 +1,19 @@
-import math
+"""
+Class to implement square root function without using sqrt function in math library
+"""
 class Sqrt:
+    """
+    Parameter:
+        number: number whose square root need to be calculated
+    Returns:
+        Floor value of the square root
+    """
+    # The square root lies between [1, number]
+    # Initial guess is 1. If the square of the result computed is too far off from the given number then iterate
+    # Iteration uses 0.5 * (start + number / start) as next guess
+    # Iteration continues till convergence is achieved
     @staticmethod
     def sqrt(number, start=1.0):
         while abs(start * start - number) > 0.1:
             start = (start + number / start) * 0.5
-        return math.floor(start)
-
-if __name__ == '__main__':
-    print(Sqrt.sqrt(25))
+        return int(start)
