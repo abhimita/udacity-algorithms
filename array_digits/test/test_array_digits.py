81de42886e68ee040b6896a74a1a64d78cd4646e
abhijitbhatt <bhattacharya.abhi94@gmail.com>
2019-06-06 21:08:04 -0700

    Added more test cases

--- array_digits/test/test_array_digits.py
+++ array_digits/test/test_array_digits.py
@@ -22,7 +22,35 @@ class TestArrayDigits(unittest.TestCase):
         array = [4]
         with self.assertRaises(Exception) as context:
             ArrayDigits.rearrange_digits(array)
-            self.assertTrue('Array length is no sufficient to return two numbers' in context.exception)
+        self.assertTrue('Array length is not sufficient to return two numbers' in str(context.exception))
+
+    # Test when array contains string element
+    def test_rearrange_digits_when_one_element_is_string(self):
+        array = [4, 'a', 2, 5, 9, 8]
+        with self.assertRaises(Exception) as context:
+            ArrayDigits.rearrange_digits(array)
+        self.assertTrue('Either the array contains non integer elements or integer is more than 9' in str(context.exception))
+
+    # Test when array contains float
+    def test_rearrange_digits_when_one_element_is_float(self):
+        array = [4, 3.0, 2, 5, 9, 8]
+        with self.assertRaises(Exception) as context:
+            ArrayDigits.rearrange_digits(array)
+        self.assertTrue('Either the array contains non integer elements or integer is more than 9' in str(context.exception))
+
+    # Test when array is empty
+    def test_rearrange_digits_when_array_is_empty(self):
+        array = []
+        with self.assertRaises(Exception) as context:
+            ArrayDigits.rearrange_digits(array)
+        self.assertTrue('Array length is not sufficient to return two numbers' in str(context.exception))
+
+    # Test when array contains two digited element as integer
+    def test_rearrange_digits_when_one_element_is_two_digited(self):
+        array = [4, 30, 2, 5, 9, 8]
+        with self.assertRaises(Exception) as context:
+            ArrayDigits.rearrange_digits(array)
+        self.assertTrue('Either the array contains non integer elements or integer is more than 9' in str(context.exception))
 
 if __name__ == '__main__':
     suite = unittest.TestLoader().loadTestsFromTestCase(TestArrayDigits)
